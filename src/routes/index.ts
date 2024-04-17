49a9bb4e1b719005d77344a39a5f0b50cb5dbfde
marcelo <sjsjsjmarcelo@gmail.com>
2024-04-17 17:19:53 -0300

    Created session routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,7 @@ import { userRoutes } from "./users.routes";
 import { alunoRoutes } from "./alunos.routes";
 import { livroRoutes } from "./livros.routes";
 import { emprestimoRoutes } from "./emprestimos.routes";
+import { sessionRoutes } from "./sessions.routes";
 
 const routes = Router();
 
@@ -11,5 +12,6 @@ routes.use('/alunos', alunoRoutes);
 routes.use('/livros', livroRoutes);
 routes.use('/emprestimos', emprestimoRoutes);
 routes.use('/users', userRoutes);
+routes.use('/sessions', sessionRoutes)
 
 export { routes };
